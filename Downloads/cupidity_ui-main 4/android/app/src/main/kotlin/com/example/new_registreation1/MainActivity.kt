4b10f85a8d355e1c0fc6139d7557c29ccb39a5a1
ankitmishra1201 <75842497+ankitmishra1201@users.noreply.github.com>
2021-10-03 20:02:06 +0530

    Delete Downloads/cupidity_ui-main 4 directory

--- Downloads/cupidity_ui-main 4/android/app/src/main/kotlin/com/example/new_registreation1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.new_registreation1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Downloads/cupidity_ui-main 4/lib/pages/profile_page/about_me/assets.dart
+++
@@ -1,3 +0,0 @@
-const List<String> avatars = [
-  'https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/img%2F1.jpg?alt=media',
-];
